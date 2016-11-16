f8fc45cbcbb8253918fd398e2d1b9c089183804e
angelozerr <angelo.zerr@gmail.com>
2016-11-16 23:20:22 +0100

    clean

--- eclipse/fr.opensagres.language.textmate.eclipse/src/main/java/fr/opensagres/language/textmate/eclipse/text/TMPresentationReconciler.java
+++ eclipse/fr.opensagres.language.textmate.eclipse/src/main/java/fr/opensagres/language/textmate/eclipse/text/TMPresentationReconciler.java
@@ -154,12 +154,6 @@ public class TMPresentationReconciler implements IPresentationReconciler {
 		return grammar;
 	}
 
-//	private IFile getFile(IDocument document) {
-//		ITextFileBufferManager bufferManager = FileBuffers.getTextFileBufferManager();
-//		ITextFileBuffer fileBuffer = bufferManager.getTextFileBuffer(document);
-//		return ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(fileBuffer.getLocation());
-//	}
-//	
 	/**
 	 * Returns the file from the given {@link IDocument}.
 	 */
