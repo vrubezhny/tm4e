55f5bd430f4a89c51a73e5fc119df8739d3f69c1
angelozerr <angelo.zerr@gmail.com>
2017-01-05 21:56:32 +0100

    Add Theme Id constants

---
+++ org.eclipse.tm4e.ui/src/main/java/org/eclipse/tm4e/ui/themes/ThemeIdConstants.java
@@ -0,0 +1,24 @@
+/**
+ *  Copyright (c) 2015-2017 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
+package org.eclipse.tm4e.ui.themes;
+
+/**
+ * Default TextMate theme ID constants.
+ *
+ */
+public interface ThemeIdConstants {
+
+	public static final String Light = "org.eclipse.tm4e.ui.themes.Light";
+	public static final String Monokai = "org.eclipse.tm4e.ui.themes.Monokai";
+	public static final String SolarizedLight = "org.eclipse.tm4e.ui.themes.SolarizedLight";
+	public static final String Dark = "org.eclipse.tm4e.ui.themes.Dark";
+
+}
