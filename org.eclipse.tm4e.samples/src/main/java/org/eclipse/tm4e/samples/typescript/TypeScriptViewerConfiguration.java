ec89c98cb4346a6b274c69a5e87548dd1de1c535
Alexander Kurtakov <akurtako@redhat.com>
2018-02-09 14:13:26 +0200

    Remove unused imports.
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- org.eclipse.tm4e.samples/src/main/java/org/eclipse/tm4e/samples/typescript/TypeScriptViewerConfiguration.java
+++ org.eclipse.tm4e.samples/src/main/java/org/eclipse/tm4e/samples/typescript/TypeScriptViewerConfiguration.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 2015-2017 Angelo ZERR.
+ *  Copyright (c) 2015-2018 Angelo ZERR.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -15,9 +15,7 @@ import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
 import org.eclipse.tm4e.core.grammar.IGrammar;
 import org.eclipse.tm4e.core.registry.Registry;
-import org.eclipse.tm4e.ui.TMUIPlugin;
 import org.eclipse.tm4e.ui.text.TMPresentationReconciler;
-import org.eclipse.tm4e.ui.themes.ThemeIdConstants;
 
 public class TypeScriptViewerConfiguration extends SourceViewerConfiguration {
 
