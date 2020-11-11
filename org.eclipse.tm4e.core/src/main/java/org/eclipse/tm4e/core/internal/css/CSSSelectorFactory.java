547a12fc906cbb5d383bd736b1d1b3fcbd30d381
Jochen Ulrich <jochenulrich@t-online.de>
2020-11-11 21:51:47 +0100

    Adds exception messages
    Signed-off-by: Jochen Ulrich <jochenulrich@t-online.de>

--- org.eclipse.tm4e.core/src/main/java/org/eclipse/tm4e/core/internal/css/CSSSelectorFactory.java
+++ org.eclipse.tm4e.core/src/main/java/org/eclipse/tm4e/core/internal/css/CSSSelectorFactory.java
@@ -8,6 +8,7 @@
  *
  *  Contributors:
  *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *  Jochen Ulrich <jochenulrich@t-online.de> - exception messages
  */
 package org.eclipse.tm4e.core.internal.css;
 
@@ -30,22 +31,22 @@ public class CSSSelectorFactory implements SelectorFactory {
 
 	@Override
 	public SimpleSelector createAnyNodeSelector() throws CSSException {
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("CSS any selector is not supported");
 	}
 
 	@Override
 	public CharacterDataSelector createCDataSectionSelector(String arg0) throws CSSException {
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("CSS CDATA section is not supported");
 	}
 
 	@Override
 	public DescendantSelector createChildSelector(Selector arg0, SimpleSelector arg1) throws CSSException {
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("CSS child selector is not supported");
 	}
 
 	@Override
 	public CharacterDataSelector createCommentSelector(String arg0) throws CSSException {
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("CSS comment is not supported");
 	}
 
 	@Override
@@ -56,13 +57,13 @@ public class CSSSelectorFactory implements SelectorFactory {
 
 	@Override
 	public DescendantSelector createDescendantSelector(Selector arg0, SimpleSelector arg1) throws CSSException {
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("CSS descendant selector is not supported");
 	}
 
 	@Override
 	public SiblingSelector createDirectAdjacentSelector(short arg0, Selector arg1, SimpleSelector arg2)
 			throws CSSException {
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("CSS direct adjacent selector is not supported");
 	}
 
 	@Override
@@ -72,28 +73,28 @@ public class CSSSelectorFactory implements SelectorFactory {
 
 	@Override
 	public NegativeSelector createNegativeSelector(SimpleSelector arg0) throws CSSException {
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("CSS negative selector is not supported");
 	}
 
 	@Override
 	public ProcessingInstructionSelector createProcessingInstructionSelector(String arg0, String arg1)
 			throws CSSException {
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("CSS processing instruction is not supported");
 	}
 
 	@Override
 	public ElementSelector createPseudoElementSelector(String arg0, String arg1) throws CSSException {
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("CSS pseudo element selector is not supported");
 	}
 
 	@Override
 	public SimpleSelector createRootNodeSelector() throws CSSException {
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("CSS root node selector is not supported");
 	}
 
 	@Override
 	public CharacterDataSelector createTextNodeSelector(String arg0) throws CSSException {
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("CSS text node selector is not supported");
 	}
 
 }
