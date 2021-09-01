67022ba32b4a335439d3cd025560e16e418b3cd8
Alexander Kurtakov <akurtako@redhat.com>
2021-09-01 10:45:38 +0300

    Fix try-with-resources warning

--- org.eclipse.tm4e.core/src/test/java/org/eclipse/tm4e/core/grammar/MarkDown.java
+++ org.eclipse.tm4e.core/src/test/java/org/eclipse/tm4e/core/grammar/MarkDown.java
@@ -62,7 +62,8 @@ public class MarkDown {
 	}
 	
 	static String convertStreamToString(java.io.InputStream is) {
-	    java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-	    return s.hasNext() ? s.next() : "";
+	    try (java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A")) {
+	    	return s.hasNext() ? s.next() : "";
+	    }
 	}
 }
