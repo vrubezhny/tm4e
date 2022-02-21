9b79be4154ae198ab8de1f2ddc586792a01ed0b4
sebthom <sebthom@users.noreply.github.com>
2022-02-21 17:12:07 +0100

    add missing hashCode() method

--- org.eclipse.tm4e.core.tests/src/main/java/org/eclipse/tm4e/core/grammar/internal/RawToken.java
+++ org.eclipse.tm4e.core.tests/src/main/java/org/eclipse/tm4e/core/grammar/internal/RawToken.java
@@ -1,13 +1,13 @@
 /**
- *  Copyright (c) 2015-2017 Angelo ZERR.
+ * Copyright (c) 2015-2017 Angelo ZERR.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
  *
- *  Contributors:
- *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ * Contributors:
+ * Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  */
 package org.eclipse.tm4e.core.grammar.internal;
 
@@ -44,5 +44,14 @@ public class RawToken {
 			return false;
 		}
 		return this.scopes.equals(other.scopes);
+	}
+
+	@Override
+	public int hashCode() {
+		final var prime = 31;
+		var result = 1;
+		result = prime * result + (scopes == null ? 0 : scopes.hashCode());
+		result = prime * result + (value == null ? 0 : value.hashCode());
+		return result;
 	}
 }
