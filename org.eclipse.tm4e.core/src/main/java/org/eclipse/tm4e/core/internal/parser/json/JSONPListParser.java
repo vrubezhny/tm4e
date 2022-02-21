c081ad04b3aed0691e39944b5ec3b62b7ddb5c0e
sebthom <sebthom@users.noreply.github.com>
2022-02-21 16:57:19 +0100

    Use try-with-resources with JsonReader

--- org.eclipse.tm4e.core/src/main/java/org/eclipse/tm4e/core/internal/parser/json/JSONPListParser.java
+++ org.eclipse.tm4e.core/src/main/java/org/eclipse/tm4e/core/internal/parser/json/JSONPListParser.java
@@ -30,57 +30,57 @@ public class JSONPListParser<T> {
 
 	public T parse(InputStream contents) throws Exception {
 		PList<T> pList = new PList<>(theme);
-		JsonReader reader = new JsonReader(new InputStreamReader(contents, StandardCharsets.UTF_8));
-		// reader.setLenient(true);
-		boolean parsing = true;
-		while (parsing) {
-			JsonToken nextToken = reader.peek();
-			switch (nextToken) {
-			case BEGIN_ARRAY:
-				pList.startElement(null, "array", null, null);
-				reader.beginArray();
-				break;
-			case END_ARRAY:
-				pList.endElement(null, "array", null);
-				reader.endArray();
-				break;
-			case BEGIN_OBJECT:
-				pList.startElement(null, "dict", null, null);
-				reader.beginObject();
-				break;
-			case END_OBJECT:
-				pList.endElement(null, "dict", null);
-				reader.endObject();
-				break;
-			case NAME:
-				String lastName = reader.nextName();
-				pList.startElement(null, "key", null, null);
-				pList.characters(lastName.toCharArray(), 0, lastName.length());
-				pList.endElement(null, "key", null);
-				break;
-			case NULL:
-				reader.nextNull();
-				break;
-			case BOOLEAN:
-				reader.nextBoolean();
-				break;
-			case NUMBER:
-				reader.nextLong();
-				break;
-			case STRING:
-				String value = reader.nextString();
-				pList.startElement(null, "string", null, null);
-				pList.characters(value.toCharArray(), 0, value.length());
-				pList.endElement(null, "string", null);
-				break;
-			case END_DOCUMENT:
-				parsing = false;
-				break;
-			default:
-				break;
+		try (JsonReader reader = new JsonReader(new InputStreamReader(contents, StandardCharsets.UTF_8))) {
+			// reader.setLenient(true);
+			boolean parsing = true;
+			while (parsing) {
+				JsonToken nextToken = reader.peek();
+				switch (nextToken) {
+				case BEGIN_ARRAY:
+					pList.startElement(null, "array", null, null);
+					reader.beginArray();
+					break;
+				case END_ARRAY:
+					pList.endElement(null, "array", null);
+					reader.endArray();
+					break;
+				case BEGIN_OBJECT:
+					pList.startElement(null, "dict", null, null);
+					reader.beginObject();
+					break;
+				case END_OBJECT:
+					pList.endElement(null, "dict", null);
+					reader.endObject();
+					break;
+				case NAME:
+					String lastName = reader.nextName();
+					pList.startElement(null, "key", null, null);
+					pList.characters(lastName.toCharArray(), 0, lastName.length());
+					pList.endElement(null, "key", null);
+					break;
+				case NULL:
+					reader.nextNull();
+					break;
+				case BOOLEAN:
+					reader.nextBoolean();
+					break;
+				case NUMBER:
+					reader.nextLong();
+					break;
+				case STRING:
+					String value = reader.nextString();
+					pList.startElement(null, "string", null, null);
+					pList.characters(value.toCharArray(), 0, value.length());
+					pList.endElement(null, "string", null);
+					break;
+				case END_DOCUMENT:
+					parsing = false;
+					break;
+				default:
+					break;
+				}
 			}
 		}
-		reader.close();
 		return pList.getResult();
 	}
 
