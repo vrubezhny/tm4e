0d22b683284f60949eb30e826a7564d80d8f031e
sebthom <sebthom@users.noreply.github.com>
2022-04-11 23:59:55 +0200

    Cleanup JSONPlistParserTest and XMLPlistParserTest
    - use try-with-resources to fix resource leak
    - use proper name for test methods
    - add some assertions to test methods
    - remove duplicate test plist resources

--- org.eclipse.tm4e.core/src/test/java/org/eclipse/tm4e/core/internal/grammar/parser/json/JSONPListParserTest.java
+++ org.eclipse.tm4e.core/src/test/java/org/eclipse/tm4e/core/internal/grammar/parser/json/JSONPListParserTest.java
@@ -1,27 +1,37 @@
 /**
- *  Copyright (c) 2015-2017 Angelo ZERR.
+ * Copyright (c) 2015-2017 Angelo ZERR.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
  *
- *  Contributors:
- *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ * Contributors:
+ * Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  */
 package org.eclipse.tm4e.core.internal.grammar.parser.json;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.InputStream;
+
+import org.eclipse.tm4e.core.Data;
 import org.eclipse.tm4e.core.internal.grammar.reader.GrammarReader;
 import org.eclipse.tm4e.core.internal.grammar.reader.IGrammarParser;
 import org.eclipse.tm4e.core.internal.types.IRawGrammar;
+
 import org.junit.jupiter.api.Test;
 
 public class JSONPListParserTest {
 
 	@Test
-	public void testCsharp() throws Exception {
+	public void testParseJSONPList() throws Exception {
 		IGrammarParser parser = GrammarReader.JSON_PARSER;
-		IRawGrammar grammar = parser.parse(JSONPListParserTest.class.getResourceAsStream("csharp.json"));
-		System.err.println(grammar);
+		try (InputStream is = Data.class.getResourceAsStream("csharp.json")) {
+			IRawGrammar grammar = parser.parse(is);
+			assertNotNull(grammar);
+			assertFalse(grammar.getFileTypes().isEmpty());
+			System.err.println(grammar);
+		}
 	}
 }
--- org.eclipse.tm4e.core/src/test/java/org/eclipse/tm4e/core/internal/grammar/parser/xml/XMLPlistParserTest.java
+++ org.eclipse.tm4e.core/src/test/java/org/eclipse/tm4e/core/internal/grammar/parser/xml/XMLPlistParserTest.java
@@ -1,16 +1,21 @@
 /**
- *  Copyright (c) 2015-2017 Angelo ZERR.
+ * Copyright (c) 2015-2017 Angelo ZERR.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
  *
- *  Contributors:
- *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ * Contributors:
+ * Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  */
 package org.eclipse.tm4e.core.internal.grammar.parser.xml;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.InputStream;
+
+import org.eclipse.tm4e.core.Data;
 import org.eclipse.tm4e.core.internal.grammar.reader.GrammarReader;
 import org.eclipse.tm4e.core.internal.grammar.reader.IGrammarParser;
 import org.eclipse.tm4e.core.internal.types.IRawGrammar;
@@ -19,9 +24,13 @@ import org.junit.jupiter.api.Test;
 public class XMLPlistParserTest {
 
 	@Test
-	public void testCsharp() throws Exception {
+	public void testParseXMLPlist() throws Exception {
 		IGrammarParser parser = GrammarReader.XML_PARSER;
-		IRawGrammar grammar = parser.parse(XMLPlistParserTest.class.getResourceAsStream("JavaScript.tmLanguage"));
-		System.err.println(grammar);
+		try (InputStream is = Data.class.getResourceAsStream("JavaScript.tmLanguage")) {
+			IRawGrammar grammar = parser.parse(is);
+			assertNotNull(grammar);
+			assertFalse(grammar.getFileTypes().isEmpty());
+			System.err.println(grammar);
+		}
 	}
 }
