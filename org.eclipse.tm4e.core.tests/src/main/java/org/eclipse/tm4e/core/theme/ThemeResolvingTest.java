10cad94ee99734baf305722ce6c20b2364e4e4dd
sebthom <sebthom@users.noreply.github.com>
2022-05-02 22:40:22 +0200

    add port of ThemeParsingCanParse test case

--- org.eclipse.tm4e.core.tests/src/main/java/org/eclipse/tm4e/core/theme/ThemeResolvingTest.java
+++ org.eclipse.tm4e.core.tests/src/main/java/org/eclipse/tm4e/core/theme/ThemeResolvingTest.java
@@ -48,6 +48,44 @@ public class ThemeResolvingTest {
 	}
 
 	@Test
+	public void testThemeParsingCanParse() throws Exception {
+		var actual = parseTheme(("{" +
+				"'settings': [" +
+				"{ 'settings': { 'foreground': '#F8F8F2', 'background': '#272822' } }," +
+				"{ 'scope': 'source, something', 'settings': { 'background': '#100000' } }," +
+				"{ 'scope': ['bar', 'baz'], 'settings': { 'background': '#010000' } }," +
+				"{ 'scope': 'source.css selector bar', 'settings': { 'fontStyle': 'bold' } }," +
+				"{ 'scope': 'constant', 'settings': { 'fontStyle': 'italic', 'foreground': '#ff0000' } }," +
+				"{ 'scope': 'constant.numeric', 'settings': { 'foreground': '#00ff00' } }," +
+				"{ 'scope': 'constant.numeric.hex', 'settings': { 'fontStyle': 'bold' } }," +
+				"{ 'scope': 'constant.numeric.oct', 'settings': { 'fontStyle': 'bold italic underline' } }," +
+				"{ 'scope': 'constant.numeric.bin', 'settings': { 'fontStyle': 'bold strikethrough' } }," +
+				"{ 'scope': 'constant.numeric.dec', 'settings': { 'fontStyle': '', 'foreground': '#0000ff' } }," +
+				"{ 'scope': 'foo', 'settings': { 'fontStyle': '', 'foreground': '#CFA' } }" +
+				"]" +
+				"}").replace('\'', '"'));
+
+		var expected = List.of(
+				new ParsedThemeRule("", null, 0, FontStyle.NotSet, "#F8F8F2", "#272822"),
+				new ParsedThemeRule("source", null, 1, FontStyle.NotSet, null, "#100000"),
+				new ParsedThemeRule("something", null, 1, FontStyle.NotSet, null, "#100000"),
+				new ParsedThemeRule("bar", null, 2, FontStyle.NotSet, null, "#010000"),
+				new ParsedThemeRule("baz", null, 2, FontStyle.NotSet, null, "#010000"),
+				new ParsedThemeRule("bar", List.of("selector", "source.css"), 3, FontStyle.Bold, null, null),
+				new ParsedThemeRule("constant", null, 4, FontStyle.Italic, "#ff0000", null),
+				new ParsedThemeRule("constant.numeric", null, 5, FontStyle.NotSet, "#00ff00", null),
+				new ParsedThemeRule("constant.numeric.hex", null, 6, FontStyle.Bold, null, null),
+				new ParsedThemeRule("constant.numeric.oct", null, 7,
+						FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, null, null),
+				new ParsedThemeRule("constant.numeric.bin", null, 8, FontStyle.Bold | FontStyle.Strikethrough, null,
+						null),
+				new ParsedThemeRule("constant.numeric.dec", null, 9, FontStyle.None, "#0000ff", null),
+				new ParsedThemeRule("foo", null, 10, FontStyle.None, "#CFA", null));
+
+		assertArrayEquals(expected.toArray(), actual.toArray());
+	}
+
+	@Test
 	public void testStrcmpWorks() {
 		var actual = Arrays.asList("bar", "z", "zu", "a", "ab", "");
 		actual.sort(CompareUtils::strcmp);
