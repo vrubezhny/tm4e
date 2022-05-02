146bfc99abb4b6427a07799a2085ad98bffc9227
sebthom <sebthom@users.noreply.github.com>
2022-05-02 16:21:42 +0200

    Make XML parser invulnerable to XXE attacks

--- org.eclipse.tm4e.core/src/main/java/org/eclipse/tm4e/core/internal/parser/PListParserXML.java
+++ org.eclipse.tm4e.core/src/main/java/org/eclipse/tm4e/core/internal/parser/PListParserXML.java
@@ -15,11 +15,13 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
 public final class PListParserXML<T> implements PListParser<T> {
 
@@ -33,7 +35,18 @@ public final class PListParserXML<T> implements PListParser<T> {
 	public T parse(final InputStream contents) throws IOException, ParserConfigurationException, SAXException {
 		final var spf = SAXParserFactory.newInstance();
 		spf.setNamespaceAware(true);
-		final var xmlReader = spf.newSAXParser().getXMLReader();
+
+		// make parser invulnerable to XXE attacks, see https://rules.sonarsource.com/java/RSPEC-2755
+		spf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+		spf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+
+		final var saxParser = spf.newSAXParser();
+
+		// make parser invulnerable to XXE attacks, see https://rules.sonarsource.com/java/RSPEC-2755
+		saxParser.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+		saxParser.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+
+		final XMLReader xmlReader = saxParser.getXMLReader();
 		xmlReader.setEntityResolver((publicId, systemId) -> new InputSource(
 				new ByteArrayInputStream("<?xml version='1.0' encoding='UTF-8'?>".getBytes())));
 		final var result = new PListParserContentHandler<T>(mapFactory);
