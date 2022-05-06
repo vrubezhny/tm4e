cd767c904242907a413f58e8bfc4dc9008384836
sebthom <sebthom@users.noreply.github.com>
2022-05-06 13:20:06 +0200

    add multi-line yaml test

--- org.eclipse.tm4e.core/src/test/java/org/eclipse/tm4e/core/grammar/GrammarTest2.java
+++ org.eclipse.tm4e.core/src/test/java/org/eclipse/tm4e/core/grammar/GrammarTest2.java
@@ -1,22 +1,23 @@
 /**
- *  Copyright (c) 2015-2017 Angelo ZERR.
+ * Copyright (c) 2015-2017 Angelo ZERR.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
  *
- *  Contributors:
- *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ * Contributors:
+ * Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  */
 package org.eclipse.tm4e.core.grammar;
 
 import static org.eclipse.tm4e.core.internal.utils.NullSafetyHelper.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import org.eclipse.tm4e.core.Data;
 import org.eclipse.tm4e.core.registry.Registry;
@@ -30,42 +31,60 @@ class GrammarTest2 {
 
 	@Test
 	void tokenizeLines() throws Exception {
-		Registry registry = new Registry();
-		String path = "JavaScript.tmLanguage";
-		IGrammar grammar = castNonNull(registry.loadGrammarFromPathSync(path, Data.class.getResourceAsStream(path)));
+		final var registry = new Registry();
+		final var path = "JavaScript.tmLanguage";
+		try (var is = Data.class.getResourceAsStream(path)) {
+			final var grammar = castNonNull(registry.loadGrammarFromPathSync(path, is));
 
-		IStackElement ruleStack = null;
-		int i = 0;
+			IStackElement ruleStack = null;
+			int i = 0;
 
-		List<String> lines = new ArrayList<>();
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(Data.class.getResourceAsStream("raytracer.ts")));){
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				lines.add(line);
+			final var lines = new ArrayList<String>();
+			try (var reader = new BufferedReader(
+					new InputStreamReader(Data.class.getResourceAsStream("raytracer.ts")));) {
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					lines.add(line);
+				}
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+
+			int t = 0;
+			boolean stop = false;
+			long start = System.currentTimeMillis();
+			for (String line : lines) {
+				ITokenizeLineResult lineTokens = grammar.tokenizeLine(line, ruleStack);
+				if (stop) {
+					t = 1000;
+					Thread.sleep(t);
+					stop = false;
+				}
+				ruleStack = lineTokens.getRuleStack();
+				for (i = 0; i < lineTokens.getTokens().length; i++) {
+					IToken token = lineTokens.getTokens()[i];
+					String s = "Token from " + token.getStartIndex() + " to " + token.getEndIndex() + " with scopes "
+							+ token.getScopes();
+					// System.err.println(s);
+					// Assert.assertEquals(EXPECTED_MULTI_LINE_TOKENS[i + j], s);
+				}
+			}
+			System.out.println(System.currentTimeMillis() - start - t);
 		}
+	}
 
-		int t = 0;
-		boolean stop = false;
-		long start = System.currentTimeMillis();
-		for (String line : lines) {
-			ITokenizeLineResult lineTokens = grammar.tokenizeLine(line, ruleStack);
-			if (stop) {
-				t = 1000;
-				Thread.sleep(t);
-				stop = false;
-			}
-			ruleStack = lineTokens.getRuleStack();
-			for (i = 0; i < lineTokens.getTokens().length; i++) {
-				IToken token = lineTokens.getTokens()[i];
-				String s = "Token from " + token.getStartIndex() + " to " + token.getEndIndex() + " with scopes "
-						+ token.getScopes();
-				System.err.println(s);
-				// Assert.assertEquals(EXPECTED_MULTI_LINE_TOKENS[i + j], s);
-			}
+	@Test
+	public void testYamlMultiline() throws Exception {
+		final var registry = new Registry();
+		final var path = "yaml.tmLanguage.json";
+		try (var in = Data.class.getResourceAsStream(path)) {
+			final var grammar = castNonNull(registry.loadGrammarFromPathSync(path, in));
+			String lines = ">\n should.be.string.unquoted.block.yaml\n should.also.be.string.unquoted.block.yaml";
+			final var result = TokenizationUtils.tokenizeText(lines, grammar).iterator();
+			assertTrue(Arrays.stream(result.next().getTokens())
+					.anyMatch(t -> t.getScopes().contains("keyword.control.flow.block-scalar.folded.yaml")));
+			assertTrue(Arrays.stream(result.next().getTokens())
+					.anyMatch(t -> t.getScopes().contains("string.unquoted.block.yaml")));
+			assertTrue(Arrays.stream(result.next().getTokens())
+					.anyMatch(t -> t.getScopes().contains("string.unquoted.block.yaml")));
 		}
-		System.out.println(System.currentTimeMillis() - start - t);
 	}
 }
---
+++ org.eclipse.tm4e.core/src/test/java/org/eclipse/tm4e/core/grammar/TokenizationUtils.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2022 Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * Sebastian Thomschke - initial implementation
+ */
+package org.eclipse.tm4e.core.grammar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+import com.google.common.base.Splitter;
+
+public class TokenizationUtils {
+
+	private static final Splitter BY_LINE_SPLITTER = Splitter.onPattern("\\r?\\n");
+
+	/**
+	 * Lazy tokenizes the given text.
+	 *
+	 * @param text the text to tokenize
+	 *
+	 * @return The stream of {@link ITokenizeLineResult}, each item covering 1 line of the text
+	 */
+	public static Stream<ITokenizeLineResult> tokenizeText(final CharSequence text, IGrammar grammar) {
+		if (text.length() == 0) {
+			return Stream.empty();
+		}
+
+		return BY_LINE_SPLITTER.splitToStream(text).map(new Function<String, ITokenizeLineResult>() {
+			@Nullable
+			IStackElement prevStack;
+
+			@Override
+			public ITokenizeLineResult apply(String line) {
+				final var tokenized = grammar.tokenizeLine(line, prevStack);
+				prevStack = tokenized.getRuleStack();
+				return tokenized;
+			}
+		});
+	}
+
+	/**
+	 * Lazy tokenizes the text provided by the given input stream.
+	 *
+	 * @param text the text to tokenize
+	 *
+	 * @return The stream of {@link ITokenizeLineResult}, each item covering 1 line of the text
+	 *
+	 * @throws IOException
+	 */
+	public static Stream<ITokenizeLineResult> tokenizeText(final InputStream text, IGrammar grammar)
+			throws IOException {
+		var reader = new BufferedReader(new InputStreamReader(text));
+		return reader.lines().map(new Function<String, ITokenizeLineResult>() {
+			@Nullable
+			IStackElement prevStack;
+
+			@Override
+			public ITokenizeLineResult apply(String line) {
+				final var tokenized = grammar.tokenizeLine(line, prevStack);
+				prevStack = tokenized.getRuleStack();
+				return tokenized;
+			}
+		});
+	}
+
+	private TokenizationUtils() {
+	}
+}
